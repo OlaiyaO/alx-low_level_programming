555ce603e3bc333e939d888c8d9ec42b78d01915
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-06 15:08:30 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+/*
+ * File: 5-printf.c
+ * Author: Oniya Olaiya
+ * Description: This program prints a message to the console
+ *		it serves as an example of using the `printf` function.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function is the entry point for the program.
+ *              It prints a message to the console using the `printf` function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("Last digit of %d is %d", n, abs(n) % 10);
+
+	if (abs(n) % 10 > 5)
+		printf(" and is greater than 5\n");
+	else if (abs(n) % 10 == 0)
+		printf(" and is 0\n");
+	else
+		printf(" and is less than 6 and not 0\n");
+
+	return (0);
+}
+
