7bf7c73ca4e1e9d259b84c5e4c6a978368e8d69f
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-05 17:16:31 -0700

    made another change in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
+/*
+ * 4-puts.c
+ * Oniya Olaiya
+ */
 #include <stdio.h> 
-int main(void) { puts("\"Programming is like building a multilingual puzzle"), 0; }
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle");
+       	return(0);
+}
