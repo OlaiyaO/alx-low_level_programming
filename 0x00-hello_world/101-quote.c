946abb7c5db50116cad1a2cee8c2f3044a942c60
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-05 20:24:40 -0700

    Worked on the last task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+/*
+ * File:101-quotes.c
+ * Author: Oniya Olaiya
+ * Description: This program prints a message to the console.
+ *	      It serves as an example of using the `write` function.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function is the entry point for the program.
+ *		It prints a message to the console using the `write` function.
+ * Return: Displays 1 to indicate success.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
