45384cd22d0ed915389c61fcccc4aec199a326d9
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-05 17:32:30 -0700

    Wrote a comment for the main function with betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,13 @@
  * 4-puts.c
  * Oniya Olaiya
  */
+
 #include <stdio.h>
+
+/*
+ * This is the entry point to a program that prings ""programming is like building multilingual puzzle"
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
