2561360200e607a6a980da1ba8be0b52a1f0e9d8
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-05 17:09:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
-#!/bin/bash
-int main() { puts("Programming is like building a multilingual puzzle"), 0; }
+#include <stdio.h> 
+int main(void) { puts("\"Programming is like building a multilingual puzzle"), 0; }
