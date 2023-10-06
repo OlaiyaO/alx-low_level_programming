9179700737199441680b14977eb9dd2399651ded
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-05 18:44:50 -0700

    Fixing the 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+ /*
+ * File: 5-printf.c
+ * Author: Oniya Olaiya
+ * Description: This program prints a message to the console.
+ *		it serves as an example of using the `printf` function.
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function is the entry point for the program.
+ *		It prints a message to the console using the `printf` function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
