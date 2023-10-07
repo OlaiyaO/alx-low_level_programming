10c1456a7847a73b9bd412139c3b3acb0999a417
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-06 17:48:22 -0700

    Printing hexadecimal on the screen

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,37 @@
+/*
+ * File: 2-print_alphabet.c
+ * Author: Oniya Olaiya
+ * Description: This program prints a message to the console.
+ *              it serves as an example of using the `putchar` function.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function is the entry point for the program.
+ *              It prints a message to the console using the
+ *              `putchar` function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 16)
+	{
+		if (number < 10)
+		{
+			putchar('0' + number);
+		} else
+		{
+			putchar('A' + (number - 10));
+		}
+		number++;
+	}
+	return (0);
+}
