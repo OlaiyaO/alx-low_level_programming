9c2a934fba5a93fac1df7c2e5e70d70d81e4a865
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-10 17:05:05 -0700

    2-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <ctype.h>
+#include "main.h"
+/*
+ * File: 8-24_hours.c
+ * Author: Oniya Olaiya
+ * Description: This program prints the hours and minutes of the time of a day
+ *		It checks if a number is negative and changes it to positive.
+ *
+ */
+
+
+/**
+ * jack_bauer - Entry point of the program.
+ *
+ * Description: This function takes in an integer.
+ *
+ *
+ * Return: This is a void function and it returns nothing
+ */
+
+void jack_bauer(void)
+{
+	int hours = 0;
+	
+	while (hours < 24)
+	{
+		int minutes = 0;
+
+		while (minutes < 60)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+			
+			minutes++;
+		}
+		hours++;
+	}
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,22 @@
+/*
+ * File: 8-main.c
+ * Author: Oniya Olaiya
+ * Description: This program prints the hours and minutes of a day.
+ *
+ */
+
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function is the entry point for the program.
+ *		It calls the jack bauer fction to the last digit.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
