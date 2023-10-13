3877e9bd4200117f3f37657ab67178211196b043
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-12 19:09:44 -0700

    Tried another fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Returns: 0
+ * main - Entry point. Tests function that prints text based
+ *	on whether an integer is positive or negative
+ * Return: Always 0 (Success)
  */
 
 int main(void)
