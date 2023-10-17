48a5e53450816533370b7cfdbefcc427f075bd86
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-17 11:06:01 -0700

    Tada! Congrats

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -19,25 +19,18 @@
 
 int main(void)
 {
-	int password[100];
-	int i, sum, n;
-
-	sum = 0;
+	int sum = 0;
+	int n;
 
 	srand(time(NULL));
 
-	for (i = 0; i < 100; i++)
+	while (sum < 2772 - 122)
 	{
-		password[i] = rand() % 78;
-		sum += (password[i] + '0');
-
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
+		n = rand() % 122 + 1;
+		putchar(n);
+		sum += n;
 	}
+	n = 2772 - sum;
+	putchar(n);
 	return (0);
 }
