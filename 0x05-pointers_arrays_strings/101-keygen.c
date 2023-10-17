ece4acd620c3217419d402a4561f9e42a78aea71
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-16 23:04:51 -0700

    Don't know if I'm suppsed to write a 101-crackme. program

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+/*
+ * File: 100-main.c
+ * Author: Oniya Olaiya
+ * Description: This program prints the result of _atoid.
+ */
+
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function is the entry point for the program.
+ *		It calls the print_sign function to do the checks.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	srand(time(NULL));
+
+	for (int i = 0; i < 10; i++)
+	{
+		char random_char = rand() % 93 + 33;
+
+		_putchar(random_char);
+	}
+	_putchar('\n');
+	return (0);
+}
