f38fc07585e7a0466efe638f5365991edfd0d39f
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-16 21:48:23 -0700

    done with 7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+/*
+ * File: 2-strlen.c
+ * Author: Oniya Olaiya
+ * Description: This returns the length of a string passed in.
+ *
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - Function to swap an integerm.
+ *
+ * Description: This function is the swaps 2 pointers holding.
+ *		ineger values..
+ * @str: The first integer to be swaped.
+ * Return: void (Success)
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		i = length / 2;
+	} else
+	{
+		i = (length + 1) / 2;
+	}
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
