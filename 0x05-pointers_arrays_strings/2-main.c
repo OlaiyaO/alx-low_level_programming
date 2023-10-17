626d880b06fae28b148fb708cec743b80dabf4ce
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-16 20:54:47 -0700

    done with 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,29 @@
+/*
+ * File: 1-main.c
+ * Author: Oniya Olaiya
+ * Description: This program prints swaps on the console.
+ *		It a and ba before and after swapping.
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function is the entry point for the program.
+ *		It calls the print_sign function to do the checks.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *str;
+	int len;
+
+	str = "My first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
+	return (0);
+}
