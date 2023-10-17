edcaec3dc329372c826702df8d72b0467f17115e
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-17 15:40:53 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -22,23 +22,33 @@
 
 int main(void)
 {
-	long int a = 1, b = 2, next;
-	int count = 0;
+	unsigned long int current, next, current_high;
+	unsigned long int current_low, next_high, next_low, i, j;
 
-	for (count = 1; count <= 98; count++)
+	current = 1;
+	next = 2;
+	printf("%lu", current);
+
+	for (i = 1; i < 91; i++)
 	{
-		if (count == 1)
-		{
-			printf("%ld, %lu", a, b);
-		}
-		else
-		{
-			next = a + b;
-			printf(", %lu", next);
-			a = b;
-			b = next;
-		}
+		printf(", %lu", next);
+		next = next + current;
+		current = next - current;
 	}
-	printf("/n");
+	current_high = current / 1000000000;
+	current_low = current % 1000000000;
+	next_high = next / 1000000000;
+	next_low = next % 1000000000;
+
+	for (j = 92; j < 99; ++j)
+	{
+		printf(", %lu", next_high + (next_low / 1000000000));
+		printf("%lu", next_low % 1000000000);
+		next_high = next_high + current_high;
+		current_high = next_high - current_high;
+		next_low = next_low + current_low;
+		current_low = next_low - current_low;
+	}
+	printf("\n");
 	return (0);
 }
