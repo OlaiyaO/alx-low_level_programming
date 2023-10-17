2f08e4a0a3356d424d4ae13cfa2d487dd8d4b81c
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-17 14:09:19 -0700

    working on 100_times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,41 +20,43 @@
  */
 void print_times_table(int n)
 {
-	int row = 0;
+	int i;
 
 	if (n < 0 || n > 15)
 		return;
-	while (row <= n)
+	for (i = 0; i <= n; i++)
 	{
-		int column = 1;
+		int j;
 
-		_putchar('0');
-
-		while (column <= n)
+		for (j = 0; j <= n; j++)
 		{
-			int result = row * column;
+			int result = i * j;
 
-			_putchar(',');
-			_putchar(' ');
-
-			if (result < 100)
-				_putchar(' ');
-			if (result < 10)
-				_putchar(' ');
-
-			if (result > 9 && result < 100)
+			if (j == 0)
 			{
-				_putchar((result / 10) + '0');
-			}
-			else if (result > 99)
+				_putchar(result + '0');
+			} else
 			{
-				_putchar((result/ 100) + '0');
-				_putchar(((result / 10)) % 10 + '0');
-
+				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				} else if (result < 100)
+				{
+					_putchar(' ');
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
+				} else
+				{
+					_putchar(result / 100 + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar(result % 10 + '0');
+				}
 			}
-			column++;
 		}
 		_putchar('\n');
-		row++;
 	}
 }
