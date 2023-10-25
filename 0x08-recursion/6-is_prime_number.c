c72ed32ad456664b25a90196004a6b22b4b8c450
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-25 10:17:01 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * is_prime - Helper function to check for prime.
+ * @n: The number to check.
+ * @i: The current value to test as a factor.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+
+int is_prime(int n, int i)
+{
+	if (i == n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (is_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to check.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, 2));
+}
