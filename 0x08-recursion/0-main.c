53ca1058803fd418f1e1f7495244ae73cda5c74f
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-10-25 08:01:24 -0700

    Puts with recursion

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,24 @@
+/*
+ * File: 0-main.c
+ * Author: Oniya Olaiya
+ * Description: This program prints signs to the consol.
+ *		It prints + for positive, 0 for zero and - for negative numbers.
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function is the entry point for the program.
+ *		It calls the print_sign function to do the checks.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Writes a character to stdout
+ * this file contains all the function prototype for the directory.
+ * @c: The character to be written
+ *
+ */
+void _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+
+int is_prime_number(int n);
+#endif
