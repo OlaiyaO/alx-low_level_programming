47f65461688c7b13dbc6194583c49b4f83a7f415
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-11-03 05:42:15 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,47 +1,30 @@
-/*
- * File: 1-main.c
- * Author: Oniya Olaiya
- * Description: This program changes the value pointed to by n.
- *
- */
-
 #include "main.h"
 #include <stdio.h>
 
 /**
- * _strspn - Entry point of the program.
+ * _strstr - Locates a substring within a string.
+ * @haystack: The string to search within.
+ * @needle: The substring to search for.
  *
- * Description: This function is the entry point for the program.
- *		It calls the print_sign function to do the checks.
- * @s: The destination memory location.
- * @accept: The source memory location.
- * Return: Always 0 (Success)
+ * Return: A pointer to the beginning of the located substring, or NULL if
+ * the substring is not found.
  */
 
-unsigned int _strspn(char *s, char *accept)
+char *_strstr(char *haystack, char *needle)
 {
-	unsigned int length = 0;
-
-	while (*s != '\0')
+	while (*haystack)
 	{
-		int found = 0;
-		int i;
+		char *h = haystack;
+		char *n = needle;
 
-		for (i = 0; accept[i] != '\0'; i++)
+		while (*h && *n && *h == *n)
 		{
-			if (*s == accept[i])
-			{
-				found = 1;
-				break;
-			}
+			h++;
+			n++;
 		}
-		if (found == 0)
-		{
-			return (length);
-		}
-		length++;
-		s++;
+		if (!*n)
+			return (haystack);
+		haystack++;
 	}
-	return (length);
+	return (NULL);
 }
-
