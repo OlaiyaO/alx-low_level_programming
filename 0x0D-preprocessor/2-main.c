2c86b1382a85bd20b66f46f44a65b6fb7c298d9f
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-11-06 07:04:09 -0800

    File: 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+/**
+ * File: 2-main.c
+ * Description: A C program that prints the name of the:
+ * file it was compiled from, followed by a new line.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf(__FILE__ "\n");
+    return (0);
+}
+
