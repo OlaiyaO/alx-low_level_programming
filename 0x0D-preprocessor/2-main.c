be5a76604437fb84600872bb50b893730b2b9acb
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-11-06 07:20:58 -0800

    File: 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,3 +1,11 @@
+/**
+ * main - Entry point into the program.
+ * Description: A C program that prints the name of the
+ * file it was compiled from, followed by a new line.
+ *
+ * Return: Always returns 0 success.
+ */
+
 #include <stdio.h>
 
 int main(void)
