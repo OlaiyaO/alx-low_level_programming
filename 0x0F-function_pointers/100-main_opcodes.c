485c3d185be26756323fe9ffaaa17c0e73242dcf
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-11-08 14:43:22 -0800

    File: 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,65 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "function_pointers.h"
 
 /**
- * opcodes_printer - Prints the opcodes of a function.
- * @s: Pointer to the function to print opcodes for.
+ * main - Prints opcodes of its own main function.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
  *
- * Return: No return value.
- */
-
-void opcodes_printer(char *s);
-
-/**
- * main - Entry point.
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line argument strings.
- *
- * Return: 0 on success, 1 for incorrect number of arguments,
- * 2 for negative bytes.
+ * Return: 0 on success, 1 for incorrect
+ * number of arguments, 2 for negative bytes.
  */
 
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		fprintf(stderr, "Error\n");
-		return (1);
+		printf("Error\n");
+		exit(1);
 	}
 
-	int num_bytes = atoi(argv[1]);
+	int bytes = atoi(argv[1]);
 
-	if (num_bytes < 0)
+	if (bytes < 0)
 	{
-		fprintf(stderr, "Error\n");
-		return (2);
+		printf("Error\n");
+		exit(2);
 	}
 
-	opcodes_printer((char *)&opcodes_printer);
+	char *main_address = (char *)main;
+	int i;
+
+	for (i = 0; i < bytes - 1; i++)
+	{
+		printf("%02x ", main_address[i]);
+	}
+
+	printf("%02x\n", main_address[i]);
 
 	return (0);
-}
-
-/**
- * opcodes_printer - Prints the opcodes of a function.
- * @s: Pointer to the function to print opcodes for.
- *
- * Return: No return value.
- */
-
-void opcodes_printer(char *s)
-{
-	int i;
-	for (i = 0; i < atoi(s); i++)
-	{
-		printf("%02x", s[i] & 0xFF);
-		if (i < atoi(s) - 1)
-		{
-			printf(" ");
-		} else
-		{
-			printf("\n");
-		}
-	}
 }
