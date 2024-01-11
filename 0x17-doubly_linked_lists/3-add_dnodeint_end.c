ce314aa182cfa5b7d624f98c01cad41f7187e4d8
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-11 12:10:59 +0100

    File: 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - Add a new node at the end of a doubly linked list
+ * @head: Pointer to the head of the doubly linked list
+ * @n: Value to be stored in the new node
+ * Return: Address of the new node, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *last = *head;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new_node;
+	new_node->prev = last;
+
+	return (new_node);
+}
